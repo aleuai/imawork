e52adc8c35d3be7d5b4c2adfae67625a43822b41
Aleuai <almarti.martins@gmail.com>
2012-12-10 20:37:25 -0200

    Initial rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Imawork::Application.config.secret_token = '8e6adf7edb203ff9dc3916b018fe2f0c662ddd9a0054c0c9f4dea8d8d331ab7c5d1febdaf46c15e86a758b8cccc16a92fbec83ce2e24407681d39e5533f71bc4'
